0361d75e777e10289dacdc6ebadee882e1739409
KoSangWon <jackoss@naver.com>
2020-04-14 11:39:21 +0900

    App.css 삭제 직전

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import react from 'react';
+import React, {Fragment} from 'react';
+import styled from "styled-components";
+import "./App.css"
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Container>
+      <Button success>Hello</Button>
+      <Button danger>Hello</Button>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &active,
+  &focus{
+    outline: none;
+  }
+  background-color: ${props => props.danger ? "#e74c3c" : "#2ecc71"}
+`;
 
 export default App;
